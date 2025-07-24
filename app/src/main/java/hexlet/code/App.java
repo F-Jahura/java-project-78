e60a73fd3998d11be65a3d4f8b6863f14cab968a
F-Jahura <f_jahura@yahoo.com>
2025-07-24 16:14:16 +0300

    updated

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,5 @@ package hexlet.code;
 // then press Enter. You can now see whitespace characters in your code.
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
     }
 }
