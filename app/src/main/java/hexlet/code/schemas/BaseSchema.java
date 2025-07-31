a49b072863dc92aac26509258ab5d4741647f1f8
F-Jahura <f_jahura@yahoo.com>
2025-07-31 17:00:20 +0300

    improved BaseSchema.java

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,7 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema<T> {
+public class BaseSchema<T> {
     protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
 
     protected final void addCheck(String name, Predicate<T> validate) {
